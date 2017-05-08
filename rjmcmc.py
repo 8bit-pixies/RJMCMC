7a0cf2dc2049142aa8b228c6c7a09af7e6ff6114
Chapman Siu <chpmn.siu@gmail.com>
2017-05-08 23:19:49 +1000

    update

--- rjmcmc.py
+++ rjmcmc.py
@@ -8,6 +8,10 @@ states -> some number on the real number line.
 q(x | x') = q(x' | x) for _detailed balance condition_?
 evaluation: p(x) = some objective function (say tree splits)
 
+in this code we will consider the optimal choice of tree splits 
+for unlimited splits. Obviously this is a rather contrived 
+example, because the optimal case is if there is a split for 
+every point in the dataset...
 
 """
 
@@ -15,11 +19,20 @@ evaluation: p(x) = some objective function (say tree splits)
 import numpy as np 
 from sklearn import datasets
 from scipy import stats
+import pandas as pd
 
 iris = datasets.load_iris()
 
 X = iris.data[:, 1] # take the 2nd column only
 Y = iris.target
+
+def create_bins(x, breaks):
+    """ mirror of the R implementation, create bins given breaks """
+    if not isinstance(breaks, list):
+        raise Exception('Breaks are not a list')
+        
+    breaks = sorted([float("-inf"), float("inf")] + breaks)
+    return pd.cut(x, breaks, labels=range(len(breaks)-1))
 
 def get_best_classification(Y):
     """returns the number of most frequent class 
@@ -29,16 +42,30 @@ def get_best_classification(Y):
     return stats.mode(Y)[1][0]/float(Y.shape[0])
     
 
-def get_metric(split, X, Y):
+def get_metric(splits, X, Y):
     """returns the metric related to the split, the higher the better, must 
     be between 0, 1"""
-    split_part = np.where(X > split)
-    true_part = len(split_part[0])
-    weighted_metric = (true_part * get_best_classification(Y[X>split]) + (len(Y)-true_part)*get_best_classification(Y[X<=split]))/float(len(Y))
-    return weighted_metric
-    base_metric = get_best_classification(Y)
     
-    return (weighted_metric - base_metric)/(1-base_metric)
+    if not type(splits) == list:
+        splits = [splits]
+    x_binned = create_bins(X, splits)
+    #print(x_binned)
+    
+    y_metric = 0
+    for lbl in list(set(x_binned)):
+        cond = np.where(x_binned == lbl)
+        #print(lbl)
+        #print(cond)
+        #print(Y[cond])
+        #print("\n\n")
+        y_part = len(Y[cond])
+        y_metric += y_part*get_best_classification(Y[cond])
+    
+    weighted_metric = y_metric/len(Y)
+    return weighted_metric
+    #base_metric = get_best_classification(Y)
+    #
+    #return (weighted_metric - base_metric)/(1-base_metric)
 
 #print(get_metric(2, X, Y))
 #print(get_metric(3, X, Y))
@@ -47,8 +74,6 @@ uniq_x = list(set(X.tolist()))
 map_brute = {x:get_metric(x, X, Y) for x in uniq_x}
 maximum_x = max(map_brute, key=map_brute.get)  # Just use 'min' instead of 'max' for minimum.
 print("best solution via brute force:", maximum_x, map_brute[maximum_x])
-
-
 
 # use the above for MCMC via MH, the distribution would be...normal 
 # around the point with variance of say 2...
